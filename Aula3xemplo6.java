16846da22185d7a6394db26e82fe6582897aeb97
Darth-Ren789 <37985118+Darth-Ren789@users.noreply.github.com>
2022-09-19 20:27:50 -0300

    Add files via upload

---
+++ Aula3xemplo6.java
@@ -0,0 +1,19 @@
+package aula3exemplo6;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author DarthRen
+ */
+public class Aula3xemplo6 {
+
+    public static void main(String args[]) {
+        String modelo;
+        int quilometragem;
+        float valor;
+        modelo = JOptionPane.showInputDialog(null, "Digite o modelo do veículo: ");
+        quilometragem = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite a quilometragem do veículo: "));
+        valor = Float.parseFloat(JOptionPane.showInputDialog(null, "Digite o valor do veículo em R$: "));
+        JOptionPane.showMessageDialog(null, "Dados do veículo: \n Modelo: " + modelo + "\n Quilometragem: "
+        + quilometragem + "km \n Valor em R$: " + valor);
+    }
+}
